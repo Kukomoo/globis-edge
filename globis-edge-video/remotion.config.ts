41b3b520bb3ac0c1cdc3040d20645799cdc0675a
Nadu <Nad1071996@outlook.com>
2026-05-18 22:31:37 -0600

    chore: remove globis-edge-video folder

--- globis-edge-video/remotion.config.ts
+++
@@ -1,8 +0,0 @@
-import { Config } from "remotion";
-
-Config.setCodec("h264");
-Config.setFrameRate(24);
-Config.setDimensions(1920, 1080);
-Config.setCrf(18);
-Config.setAudioCodec("aac");
-Config.setAudioBitrate("128k");
